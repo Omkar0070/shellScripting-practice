f8b949f4bd3f0d2c6459f9d5a92d9db61684822c
omkar kadam <kadamom815@gmail.com>
2023-08-12 15:06:18 +0530

    updated

--- 07_string_ops.sh
+++ 07_string_ops.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-myVar = "Hey Buddy,How are you ?"
-myVarlength =${#myVar}
+myVar="Hey Buddy,How are you ?"
+myVarlength=${#myVar}
 echo "length of the myVar is $myVarlength"
 
 echo "Upper case is --- ${myVar^^}"
 echo "Lower case is --- ${myVar,,}"
 
 #To replace a string
-newVar = ${myVar/Buddy/omkar}
+newVar=${myVar/Buddy/omkar}
 echo "new var is --- $newVar"
